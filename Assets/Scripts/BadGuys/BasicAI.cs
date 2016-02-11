fa4e9b9d01a97a2498b417c0ba0a640bd8d4759f
Jared Hill <jhill2@spsu.edu>
2016-02-10 22:08:42 -0500

    Working on AI
    the basic start of the AI

---
+++ Assets/Scripts/BadGuys/BasicAI.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicAI : MonoBehaviour {
+
+	// This variable will contain our nav mesh agent which stores a lot of our movement data
+	public NavMeshAgent agent;
+
+	// This state represents the states that the AI character can actively be in
+	public enum State {
+		PATROL,
+		CHASE,
+		ATTACK,
+		EVADE,
+		GETAMMO,
+		GETHEALTH,
+		DEATH
+	}
+
+	// We need this variable in order to change the current state of the AI
+	public State state;
+	// We need this variable to indicate whether the AI unit is currently alive or not
+	private bool alive;
+
+	// We will need these when the AI needs to be patrolling
+	public GameObject[] waypoints;
+	private int waypointInd;
+	public float patrolSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	// This is our Finite State machine which will run continuously
+	IEnumerator FSM()
+	{
+		while (alive)
+		{
+			switch (state)
+			{
+			case State.PATROL:
+				Patrol ();
+				break;
+			case State.CHASE:
+				Chase ();
+				break;
+			}
+			yield return null;
+		}
+	}
+
+	public void Patrol()
+	{
+		
+	}
+
+	public void Chase()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
