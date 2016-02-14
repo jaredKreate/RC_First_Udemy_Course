7056a551501e6187d304379e0f160c351498666d
Jared Hill <jhill2@spsu.edu>
2016-02-13 22:11:00 -0500

    fixing enemy sight for tripod
    tripod model is all kinds of messed up with the directions of the mesh.

--- Assets/Scripts/BadGuys/EnemySight.cs
+++ Assets/Scripts/BadGuys/EnemySight.cs
@@ -6,36 +6,42 @@ public class EnemySight : MonoBehaviour {
 	public bool playerSighted;
 	public float heightMultiplier;
 	public float sightDist;
+	public GameObject[] lightbeams;
 
 	// Update is called once per frame
 	void Update () {
 		if(!playerSighted)
-		{					
-			Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, -transform.up * sightDist, Color.green);
-			Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (-transform.up + transform.right) * sightDist, Color.green);
-			Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (-transform.up - transform.right) * sightDist, Color.green);
+		{
 			RaycastHit hit;
-			if(Physics.Raycast(transform.position + Vector3.up * heightMultiplier, -transform.up, out hit, sightDist))				
+			foreach(GameObject go in lightbeams)
 			{
-				if(hit.collider.gameObject.tag == "Player")
-				{					
-					playerSighted = true;
-				}
+				Debug.DrawRay(go.transform.position + Vector3.up * heightMultiplier, -go.transform.up * sightDist, Color.red);
+				if(Physics.Raycast(transform.position + Vector3.up * heightMultiplier, -go.transform.up, out hit, sightDist));
 			}
-			if(Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (-transform.up + transform.right), out hit, sightDist))				
-			{
-				if(hit.collider.gameObject.tag == "Player")
-				{					
-					playerSighted = true;
-				}
-			}
-			if(Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (-transform.up - transform.right), out hit, sightDist))				
-			{
-				if(hit.collider.gameObject.tag == "Player")
-				{					
-					playerSighted = true;
-				}
-			}
+//			Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, -transform.up * sightDist, Color.green);
+//			Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (-transform.up + transform.right) * sightDist, Color.green);
+//			Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (-transform.up - transform.right) * sightDist, Color.green);
+//			if(Physics.Raycast(transform.position + Vector3.up * heightMultiplier, -transform.up, out hit, sightDist))				
+//			{
+//				if(hit.collider.gameObject.tag == "Player")
+//				{					
+//					playerSighted = true;
+//				}
+//			}
+//			if(Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (-transform.up + transform.right), out hit, sightDist))				
+//			{
+//				if(hit.collider.gameObject.tag == "Player")
+//				{					
+//					playerSighted = true;
+//				}
+//			}
+//			if(Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (-transform.up - transform.right), out hit, sightDist))				
+//			{
+//				if(hit.collider.gameObject.tag == "Player")
+//				{					
+//					playerSighted = true;
+//				}
+//			}
 		}
 	}
 }
