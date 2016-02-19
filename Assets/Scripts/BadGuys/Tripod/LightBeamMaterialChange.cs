d100ff71a4873c280a5d9afefadc10e9e355ac78
Jared Hill <jhill2@spsu.edu>
2016-02-19 17:57:02 -0500

    Changing light beam material when player spotted

---
+++ Assets/Scripts/BadGuys/Tripod/LightBeamMaterialChange.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBeamMaterialChange : MonoBehaviour {
+
+	public Renderer renderer;
+	public Material currMat;
+	public Material newMat;
+	public TripodSight triSight;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<Renderer>();
+		currMat = GetComponent<Material>();
+		triSight = GetComponentInParent<TripodSight>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(triSight.playerSighted)
+		{
+			renderer.material = newMat;
+		}
+	}
+}
